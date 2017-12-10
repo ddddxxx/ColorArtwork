68e07b42a717b0c74b81fbb54b8b4a851881d5d2
Xander Deng <dengxiang2010@gmail.com>
2017-12-10 14:43:17 +0800

    add license for all source files

--- Sources/ColorArtwork/RGBColor.swift
+++ Sources/ColorArtwork/RGBColor.swift
@@ -1,9 +1,18 @@
 //
 //  RGBColor.swift
-//  ColorArtwork
 //
-//  Created by 邓翔 on 2017/2/24.
+//  This file is part of ColorArtwork. <https://github.com/ddddxxx/ColorArtwork>
+//  Copyright (c) 2017 Xander Deng
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 //
 
 import Foundation
